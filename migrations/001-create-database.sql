999b36e99e31c839fc9f885d2a64a4a1c035eadd
Lind-code419 <6mynameisg@gmail.com>
2023-09-06 13:48:45 +0200

    created files

---
+++ migrations/001-create-database.sql
@@ -0,0 +1,10 @@
+create table carbon_data (
+    id integer primary key AUTOINCREMENT,
+     vehicle,
+    journey_date date,
+    distance_traveled real,
+    score real,
+    co2_emitted,
+    calculated_tax
+   
+);
